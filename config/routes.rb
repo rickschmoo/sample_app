2d6f36e55b233d22929769b6a232420d134cda93
Rick Boardman <rickschmoo@gmail.com>
2011-01-05 00:12:23 -0800

    Finished Chapter 5 - layout and routes with 2 weird home page testing errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  match '/signup', :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about' 
   match '/help', :to => 'pages#help'
   
-  root :to => "welcome#index"
+  # match '/', :to => 'pages#home'
+  root :to => 'pages#home'
 
 # get "pages/home"
 #
