1356f742063342f813d70ea7886636413904e68b
Rick Boardman <rickschmoo@gmail.com>
2011-02-11 14:25:02 -0800

    Finsished Chapter 7

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -47,14 +47,43 @@ describe UsersController do
   describe "GET 'new'" do
     
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
   
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Sign up")
     end
   end
 
+
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "",
+                  :password_confirmation => "" }
+      end
+  
+      it "should not create a user" do
+        # lambda allows check of User.count
+        lambda do
+          post :create, :user => @attr
+        # change defers to the Active Record count method
+        end.should_not change(User, :count)
+      end
+      
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+ 
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+  end 
+  
 end
